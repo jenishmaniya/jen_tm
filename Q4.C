3ef2a77a37c2f9d826681f0081d6fb8f66717bc6
jenishmaniya <jenishmaniya6161@gmail.com>
2023-06-15 12:23:10 +0530

    first

--- Q4.C
+++ Q4.C
@@ -8,10 +8,10 @@ for(i=5;i>=1;i--){
    printf(" ",k);
    }
    for(j=1;j<=i;j++){
-   if((i+j)%2==0){
-   printf("1");
-   }else{
+   if(j%2==0){
    printf("0");
+   }else{
+   printf("1");
    }
    }
    printf("\n");
